882130158b72a289ac8214073f4131b0a4d75272
adamskey <adam.lennerling@gmail.com>
2022-11-29 16:47:51 +0100

    SQL connection works (json created in overview)

---
+++ front/src/app/tasklist.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: 'root'
+  })
+export class TaskListService{
+    constructor(
+        private http: HttpClient
+    ){}
+
+    getTaskList(){
+return this.http.get<{ id: number, taskCategory: number, createdTimestamp: Date, due: Date, edited: Date,
+    completed: Date, title:string, description: string, assignedTo: number, priorityId: number }[]>('localhost:8080/overview')
+    }
+
+}
