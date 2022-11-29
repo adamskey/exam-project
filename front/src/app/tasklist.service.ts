1bf9da15977e01dc9f73c6ea0be89cfa544edcfd
adamskey <adam.lennerling@gmail.com>
2022-11-29 16:47:51 +0100

    SQL connection works (json created in overview)
    # Conflicts:
    #	front/src/app/tasklist.service.ts

--- front/src/app/tasklist.service.ts
+++ front/src/app/tasklist.service.ts
@@ -10,11 +10,17 @@ export class TaskListService{
     ){}
 
     getTaskList(){
+<<<<<<< HEAD
     //return this.http.get<{ id: number, taskCategory: number, createdTimestamp: Date, due: Date, edited: Date,
     //completed: Date, title:string, description: string, assignedTo: number, priorityId: number}[]>('localhost:8080/overview')
     return this.http.get('http://localhost:8080/overview')
     }
     
     
+=======
+return this.http.get<{ id: number, taskCategory: number, createdTimestamp: Date, due: Date, edited: Date,
+    completed: Date, title:string, description: string, assignedTo: number, priorityId: number }[]>('localhost:8080/overview')
+    }
+>>>>>>> 8821301 (SQL connection works (json created in overview))
 
 }
