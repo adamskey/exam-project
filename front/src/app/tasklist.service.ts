fa9d9cd663b4c744cdbbf8c91e942239a5026c60
Emil <Emil.Johansson@awacademy.training>
2022-12-01 14:06:08 +0100

    Push to work around spring security issues

--- front/src/app/tasklist.service.ts
+++ front/src/app/tasklist.service.ts
@@ -10,9 +10,11 @@ export class TaskListService{
     ){}
 
     getTaskList(){
-    //return this.http.get<{ id: number, taskCategory: number, createdTimestamp: Date, due: Date, edited: Date,
-    //completed: Date, title:string, description: string, assignedTo: number, priorityId: number}[]>('localhost:8080/overview')
     return this.http.get('http://localhost:8080/overview')
+    }
+
+    getTaskListByUserId() {
+        return this.http.get('http://localhost:8080/overview2')
     }
     
     
