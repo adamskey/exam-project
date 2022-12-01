bb14a3d9d4b3ec63eb353b300cb17542e445c696
adamskey <adam.lennerling@gmail.com>
2022-12-01 16:04:24 +0100

    form validation at login

--- front/src/app/login/login.component.ts
+++ front/src/app/login/login.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { isEmpty } from '../string-utilities';
 
@@ -8,32 +8,38 @@ import { isEmpty } from '../string-utilities';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-  passwordHasError:boolean = false;
-  pidHasError:boolean = false;
+export class LoginComponent implements OnInit {
+  passwordHasError: boolean = false;
+  pidHasError: boolean = false;
 
 
   loginForm = this.formBuilder.group({
     pid: '',
     password: ''
   });
-  
-  
+
+
 
   constructor(
     private formBuilder: FormBuilder
 
   ) { }
   onSubmit(): void {
-    if (isEmpty(this.loginForm.value.password)){
+    if (isEmpty(this.loginForm.value.password)) {
       this.passwordHasError = true;
+    } else {
+      this.passwordHasError = false;
+    }
+    if (isEmpty(this.loginForm.value.pid)) {
+      this.pidHasError = true;
+    } else {
+      this.pidHasError = false;
+    }
 
   }
-  if (isEmpty(this.loginForm.value.pid)){
-    this.pidHasError = true;
+  ngOnInit(): void {
+      
   }
-
-}
 }
 
 
