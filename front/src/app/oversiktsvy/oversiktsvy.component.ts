329414586c3859c4d48f7696319299da2f6d3ba7
Emmelie <emmelie.wiklund@gmail.com>
2022-12-05 13:31:14 +0100

    Serach field

--- front/src/app/oversiktsvy/oversiktsvy.component.ts
+++ front/src/app/oversiktsvy/oversiktsvy.component.ts
@@ -31,8 +31,32 @@ export class OversiktsvyComponent implements OnInit {
       this.var1 = response;
     })
     console.log(this.var1)
-  }
 
+}
 
+}
 
+function searchTasks() {
+  
+  const filteredTasks = allTasks.filter(checkTask)
+
+  let html = ""
+
+  const divStrings = filteredTasks.map(createDiv)
+
+  html = divStrings.join("")
+
+  document.querySelector(".tasksResult").innerHTML = html
+
+}
+function checkTask(tasklist) {
+  return tasklist.description.includes(document.querySelector(".searchText").value)
+}
+function createDiv(g) {
+  return `<div class="row">
+                    <img src="gnomes\\${g.file}">
+                    <div class="description">${g.description}</div>
+                    <div class="price">${g.price} kr</div>
+                </div>`
+}
 }
